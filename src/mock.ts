0175efdbca0fa2eeffd1c6607b17aa13190e64b6
David <gidlos55@gmail.com>
2025-05-20 22:04:30 +0300

    2.1 - создаст главную страницу с props данными и компонентом для карточки предложения - создаст остальные страницы для приложения

---
+++ src/mock.ts
@@ -0,0 +1,32 @@
+const welcomeScreenData = {
+  foundedPlacesCount: 5,
+  email: 'gniddddddd@spb.com',
+  favoritePlacesCount: 500,
+};
+
+const cardData = {
+  //id: '6af6f711-c28d-4121-82cd-e0b462a27f00',
+  title: 'Salamalekum salaaaaaam',
+  type: 'apartment',
+  price: 120,
+  // city: {
+  //   name: 'Amsterdam',
+  //   location: {
+  //     latitude: 52.35514938496378,
+  //     longitude: 4.673877537499948,
+  //     zoom: 8,
+  //   },
+  // },
+  // location: {
+  //   latitude: 52.35514938496378,
+  //   longitude: 4.673877537499948,
+  //   zoom: 8,
+  // },
+  isFavorite: true,
+  isPremium: true,
+  // rating: 4,
+  previewImage:
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Macaca_nigra_self-portrait_large.jpg/250px-Macaca_nigra_self-portrait_large.jpg',
+};
+
+export { welcomeScreenData, cardData };
